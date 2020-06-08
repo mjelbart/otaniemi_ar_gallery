17cc13c076125b31ee80d772e589b4334cf59a1b
mjelbart <57260178+mjelbart@users.noreply.github.com>
2020-06-08 21:16:36 +0300

    heart spawning work and added some scripts from haydenjameslee

---
+++ public/js/spawn-heart.js
@@ -0,0 +1,20 @@
+// WORK IN PROGRESS
+
+AFRAME.registerComponent('spawn-heart', {
+  init: function () {
+    let sceneEl = document.querySelector('a-scene'); // the scene
+    let el = this.el; // the button
+
+    el.addEventListener('click' function () {
+      // post a message in the console to say the button was clicked
+      console.log("heart button was clicked");
+
+    var heart = document.createElement("a-entity");
+    heart.setAttribute("mixin", "heart_mixin");
+    document.querySelector("a-scene").appendChild(heart);
+
+
+      });
+
+    }
+  });
